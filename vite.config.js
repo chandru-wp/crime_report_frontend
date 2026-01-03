2100befaf1e968d9d5456e48a57fcb0dd4519b1c
Chandru <ck6582293@gmail.com>
2026-01-03 11:52:44 +0530

    vite error fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Crime_report_frontend/',
+  base: '/',
   plugins: [react(),tailwindcss()],
   build: {
     chunkSizeWarningLimit: 1000,
